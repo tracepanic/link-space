bf5a236e6e80fcf0bc96fe68b11e9390ff7bd102
tracepanic <patrickscript@proton.me>
2025-04-11 14:15:02 +0300

    Fix prisma

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+  webpack: (config) => {
+    config.externalsPresets = { node: true };
+    return config;
+  },
 };
 
 export default nextConfig;
