79ad1a676da671b7cb1532582abd9968b8c4cf7e
tracepanic <patrickscript@proton.me>
2025-04-11 18:32:32 +0300

    Sync clerk theme with nextjs theme

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,17 @@
 import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function Page() {
+  const { resolvedTheme } = useTheme();
+
   return (
     <div className="container mx-auto px-4 mt-10 flex justify-center">
-      <SignIn />
+      <SignIn
+        appearance={{
+          baseTheme: resolvedTheme === "dark" ? dark : undefined,
+        }}
+      />
     </div>
   );
 }
